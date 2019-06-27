a78a7e9f33b30ff573fb08f48e3b1fa97389a537
David Ramirez <davidraviles@gmail.com>
2019-06-26 23:13:24 -0700

    Modified structure of repo list.

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 
 const RepoList = (props) => (
   <div id="list">
-    <table>
-    <tbody>
     {props.repos.map((repo, i) => {
-      return <tr key={i}>
-            <td>
-            <div className="reponame"><b><a href={repo.html_url}>{repo.repo_name}</a></b></div>
-            <div className="reponum">#{i+1}</div>
-            <div className="username">Username: {repo.user_login}</div>
-            <div className="forks">Forks: {repo.forks}</div>
-            </td>
-            </tr>;s
+      return <div className="repo" key={i}>
+              <div className="top">
+                <div className="reponame"><b><a href={repo.html_url}>{repo.repo_name}</a></b></div>
+                <div className="reponum">#{i+1}</div>
+              </div>
+              <div className="bottom">
+                <div className="username">Username: {repo.user_login}</div>
+                <div className="forks">Forks: {repo.forks}</div>
+              </div>
+            </div>;
     })}
-    </tbody>
-    </table>
   </div>
 )
 
